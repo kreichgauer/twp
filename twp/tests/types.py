cb67d3e09b9b3093f06b2f57c86e4adc153aee6a
Martin Kreichgauer <kreichgauer@gmail.com>
2011-11-01 09:39:42 +0100

    fixed tests

--- twp/tests/types.py
+++ twp/tests/types.py
@@ -20,26 +20,26 @@ class MessageTest(unittest.TestCase):
 		self.assertTrue(hasattr(m, "fieldA"))
 		self.assertIsNone(m.fieldA)
 		self.assertIsInstance(m._fields["fieldA"], types.String)
-		self.assertEquals(m._fields["fieldA"].name, "fieldA")
+		self.assertEqual(m._fields["fieldA"].name, "fieldA")
 		self.assertTrue(hasattr(m, "fieldB"))
 		self.assertIsNone(m.fieldB)
 		self.assertIsInstance(m._fields["fieldB"], types.Int)
-		self.assertEquals(m._fields["fieldB"].name, "_fieldB")
+		self.assertEqual(m._fields["fieldB"].name, "_fieldB")
 
 	def testIdentifierAndTag(self):
 		baseMessage = types.Message()
 		self.assertRaises(ValueError, lambda: baseMessage.identifier)
 		message = ExampleMessage()
-		self.assertEquals(message.identifier, 1)
-		self.assertEquals(message.tag, 5) # 5 + message.identifier
+		self.assertEqual(message.identifier, 1)
+		self.assertEqual(message.tag, 5) # 5 + message.identifier
 
 	def testValue(self):
 		m = ExampleMessage(fieldA='foobar')
 		m.fieldB = 42
-		self.assertEquals(m.fieldA, "foobar")
-		self.assertEquals(m._fields["fieldA"].value, "foobar")
-		self.assertEquals(m.fieldB, 42)
-		self.assertEquals(m._fields["fieldB"].value, 42)
+		self.assertEqual(m.fieldA, "foobar")
+		self.assertEqual(m._fields["fieldA"].value, "foobar")
+		self.assertEqual(m.fieldB, 42)
+		self.assertEqual(m._fields["fieldB"].value, 42)
 
 
 def runTests():
