fec0f2dd38ff4e96791dcb7d2ebfa4f228e20057
Martin Kreichgauer <kreichgauer@gmail.com>
2011-11-25 22:42:43 +0100

    parser: more ast, more generation, executable

--- twp/parser.py
+++ twp/parser.py
@@ -1,4 +1,6 @@
+import sys
 from lepl import *
+from twp import log
 
 class Protocol(Node): pass
 class MessageDef(Node): pass
@@ -10,6 +12,7 @@ class MessageDef(ProtocolElement): pass
 
 class StructDef(TypeDef): pass
 class SequenceDef(TypeDef): pass
+class CaseDef(TypeDef): pass
 class UnionDef(TypeDef): pass
 class ForwardDef(TypeDef): pass
 
@@ -36,39 +39,49 @@ with TraceVariables():
 
 	with Separator(~Whitespace()[:]):
 		type = Or(primitiveType, identifier, anyDefinedByType)
-		idPair = id & number
+		idPair = ~id & number > "extension_id"
 
 		field = Optional("optional") & type & identifier & semicolon > Field
-		structdef = "struct" & identifier & Optional(eq & idPair) & lbr & field[1:] & rbr
-		sequencedef = "sequence" & lt & type & gt & identifier & semicolon
-		casedef = "case" & number & colon & type & identifier & semicolon
-		uniondef = "union" & identifier & lbr & casedef[1:] & rbr
-		forwarddef = "typedef" & identifier & semicolon
+		structdef = Drop("struct") & identifier & Optional(eq & idPair) & lbr & field[1:] & rbr > StructDef
+		sequencedef = "sequence" & lt & type & gt & identifier & semicolon > SequenceDef
+		casedef = "case" & number & colon & type & identifier & semicolon > CaseDef
+		uniondef = "union" & identifier & lbr & casedef[1:] & rbr > UnionDef
+		forwarddef = "typedef" & identifier & semicolon > ForwardDef
 		messageId = Regexp(r"[0-7]") >> int
-		messagedef = "message" & identifier & eq & Or(messageId > "id", idPair > "extensionId") & lbr & field[:] & rbr > MessageDef
+		messagedef = "message" & identifier & eq & Or(messageId > "id", idPair) & lbr & field[:] & rbr > MessageDef
 
 		typedef = Or(structdef, sequencedef, uniondef, forwarddef) > TypeDef
-		protocolelement = typedef | messagedef
+		protocolelement = Or(typedef, messagedef) > ProtocolElement
 
 		protocol =  Drop("protocol") & identifier & eq & ~id & (number > "id") & lbr & protocolelement[:] & rbr > Protocol
-		specification = Or(protocol, messagedef, structdef)[:]
+		specification = Or(protocol, messagedef, structdef)[:] & Eos()
 
 
 class StubGenerator(object):
 
-	def __init__(self, ast):
+	def __init__(self, ast, output_stream=sys.stdout):
 		self.ast = ast
+		self.output_stream = output_stream
 
 		self.indent_level = 0 
 		self.indent_step = 4
 		self.indent_char = " "
 		self.protocols = {}
 
-	def out(self, str):
+	def write(self, str):
+		self.output_stream.write(str)
+
+	def writeln(self, str):
 		# Remember to append a '\n' at the end.
 		if len(str):
 			str = self._indent_str() + str
-		print(str)
+		self.write(str + "\n")
+
+	def start_class(self, name, bases="object"):
+		self.writeln("")
+		if not isinstance(bases, str):
+			bases = ", ".join(bases)
+		self.writeln("class %s(%s):" % (name, bases))
 
 	def _indent_str(self):
 		return self.indent_char * self.indent_level
@@ -83,8 +96,7 @@ class StubGenerator(object):
 			raise ValueError("Indent level below 0")
 
 	def generate(self):
-		self.out("import twp")
-		self.out("")
+		self.writeln("import twp")
 		for node in self.ast:
 			self._generate_node(node)
 	
@@ -94,27 +106,38 @@ class StubGenerator(object):
 		return func(node)
 
 	def generate_protocol(self, node):
-		if hasattr(node, "TypeDef"):
-			pass # TODO implement
-
-		if hasattr(node, "MessageDef"):
-			for msg in node.MessageDef:
+		# Collect all message names
+		message_identifiers = []
+		for protocol_element in getattr(node, "ProtocolElement", []):
+			if hasattr(protocol_element, "TypeDef"):
+				typedef = protocol_element[0]
+				self._generate_node(typedef[0])
+			else:
+				msg = protocol_element[0]
+				message_identifiers.append(msg.identifier[0])
 				self._generate_node(msg)
-
-		self.out("class %sClient(twp.protocol.TWPClient):" % node.identifier[0])
+		
+		self.start_class(node.identifier[0], "twp.protocol.TWPClient")
 		self.indent()
-		self.out("protocol_id = %d" % node.id[0])
-		self.out("")
+		self.writeln("protocol_id = %d" % node.id[0])
+		self.writeln("message_types = [")
+		self.indent()
+		for msg in message_identifiers:
+			self.writeln("%s," % msg)
+		self.dedent()
+		self.writeln("]")
+		self.dedent()
 
 	def generate_messagedef(self, node):
-		self.out("class %s(twp.values.Message):" % node.identifier[0])
+		self.start_class(node.identifier[0], "twp.values.Message")
 		self.indent()
-		self.out("id = %d" % node.id[0])
-		if hasattr(node, 'Field'):
-			for field in node.Field:
-				self.generate_field(field)
+		if hasattr(node, "id"):
+			self.writeln("id = %d" % node.id[0])
+		elif hasattr(node, "extension_id"):
+			self.writeln("extension_id = %d" % node.id[0])
+		for field in getattr(node, "Field", []):
+			self.generate_field(field)
 		self.dedent()
-		self.out("")
 
 	def generate_field(self, node):
 		optional = node[0] == "optional"
@@ -133,4 +156,32 @@ class StubGenerator(object):
 			# Primitiive
 			type = node[0] if not optional else node[1]
 			field = "twp.values.%s(%s)" % (type.capitalize(), optional_str)
-		self.out('%s = %s' % (name, field))
+		self.writeln('%s = %s' % (name, field))
+
+	def generate_structdef(self, node):
+		self.start_class(node.identifier[0], "twp.values.Struct")
+		self.indent()
+		if hasattr(node, "extension_id"):
+			self.writeln("extension_id = %s" % node.extension_id[0])
+		for field in node.Field:
+			self.generate_field(field)
+		self.dedent()
+
+
+if __name__ == '__main__':
+	if len(sys.argv) == 3:
+		output = sys.argv[2]
+	elif len(sys.argv) == 2:
+		output = sys.stdout
+	else:
+		print("Usage: twp-parser <input> [<output>]")
+		exit(1)
+	input = sys.argv[1]
+	with open(input) as input, open(output, 'w') as output:
+		log.debug("Reading input file...")
+		idl = input.read()
+		log.debug("Parsing input...")
+		ast = specification.parse(idl)
+		log.debug("Generating stub...")
+		gen = StubGenerator(ast, output)
+		gen.generate()
