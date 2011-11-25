13d09b67408366ca72ac768ac51af533c3127e69
Martin Kreichgauer <kreichgauer@gmail.com>
2011-11-25 22:43:38 +0100

    add rpc idl and stub

---
+++ twp/protocols/rpc.py
@@ -0,0 +1,33 @@
+import twp
+
+class Request(twp.values.Message):
+    id = 0
+    request_id = twp.values.Int()
+    response_expected = twp.values.Int()
+    operation = twp.values.String()
+    parameters = twp.values.AnyDefinedBy("operation")
+
+class Reply(twp.values.Message):
+    id = 1
+    request_id = twp.values.Int()
+    result = twp.values.AnyDefinedBy("request_id")
+
+class CancelRequest(twp.values.Message):
+    id = 2
+    request_id = twp.values.Int()
+
+class CloseConnection(twp.values.Message):
+    id = 4
+
+class RPCException(twp.values.Struct):
+    extension_id = 3
+    text = twp.values.String()
+
+class RPC(twp.protocol.TWPClient):
+    protocol_id = 1
+    message_types = [
+        Request,
+        Reply,
+        CancelRequest,
+        CloseConnection,
+    ]
