4c7d7ff7c33dd39ed8cababcd674dc08a735a140
Martin Kreichgauer <kreichgauer@gmail.com>
2011-12-14 01:19:02 +0100

    added FAM files

---
+++ twp/protocols/fam.py
@@ -0,0 +1,44 @@
+import twp.values
+import twp.protocol
+
+class Path(twp.values.Sequence):
+    type = twp.values.String()
+
+class Changed(twp.values.Message):
+    id = 0
+    directory = Path()
+    filename = twp.values.String()
+
+class Deleted(twp.values.Message):
+    id = 1
+    directory = Path()
+    filename = twp.values.String()
+
+class Created(twp.values.Message):
+    id = 2
+    directory = Path()
+    filename = twp.values.String()
+
+class StartExecuting(twp.values.Message):
+    id = 3
+    directory = Path()
+    filename = twp.values.String()
+
+class StopExecuting(twp.values.Message):
+    id = 4
+    directory = Path()
+    filename = twp.values.String()
+
+class FAM(twp.protocol.Protocol):
+    protocol_id = 4
+    message_types = [
+        Changed,
+        Deleted,
+        Created,
+        StartExecuting,
+        StopExecuting,
+    ]
+
+class FAMConsumer(twp.protocol.TWPConsumer):
+    protocol_class = FAM
+    pass
